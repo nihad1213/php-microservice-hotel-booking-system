018a47d2c3caed095bccd16bd997efa56f804a62
Nihad <nihad.nemetli@gmail.com>
2025-11-17 16:32:49 +0400

    add: autload.php added

--- api-gateway/index.php
+++ api-gateway/index.php
@@ -2,12 +2,13 @@
 
 declare(strict_types=1);
 
+require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/config.php';
+
 header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type, Authorization');
-
-require_once __DIR__ . '/config.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
