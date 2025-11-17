fd01b840d61b29356ddb64f06677a702899ae4ad
Nihad <nihad.nemetli@gmail.com>
2025-11-17 16:30:15 +0400

    new: routing library installed

--- api-gateway/index.php
+++ api-gateway/index.php
@@ -1,1 +1,15 @@
-first
+<?php
+
+declare(strict_types=1);
+
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
+
+require_once __DIR__ . '/config.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
