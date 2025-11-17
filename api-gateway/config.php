5a9f2e2ef5097e444ec20b61e4ca44cad7beccf3
Nihad <nihad.nemetli@gmail.com>
2025-11-17 15:34:56 +0400

    new: config.php file in api-gateway completed

--- api-gateway/config.php
+++ api-gateway/config.php
@@ -1,1 +1,11 @@
-start
+<?php
+
+// Service URLs
+define('SERVICE_URLS', [
+    'booking' => 'http://localhost:8000',
+    'property' => 'http://localhost:8001',
+    'payment' => 'http://localhost:8002',
+    'review' => 'http://localhost:8003',
+    'search' => 'http://localhost:8004',
+    'user' => 'http://localhost:8005',
+]);
